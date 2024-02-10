7fcb404e68a831d36ba585999bb096730498d09f
Deevanshu Kushwah <astroboiscosmos@gmail.com>
2024-02-11 01:05:36 +0530

    UPDATE: createQueue_links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpLoaderService } from './services/http-loader.service';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, ProgressSpinnerModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'QMS_frontend';
 
-  constructor(public httpLoaderService: HttpLoaderService){}
+  constructor(
+    public httpLoaderService: HttpLoaderService,
+    private primengConfig: PrimeNGConfig
+  ) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
